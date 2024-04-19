f69e5df57d825aaecdb552f117e0891c56976c8b
Punthara Thirani <135725809+alpthirani@users.noreply.github.com>
2024-04-19 13:33:50 +0530

    Add files via upload

---
+++ Most_popular_books/Most_popular_books/Most_popular_books/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace Most_popular_books
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.B_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.B_TITLE = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.B_AUTHOR = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.B_CATEGORY = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.B_PRICE = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CHOISE = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Navy;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(294, 584);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(108, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Book Shop";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(52, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Categories";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(52, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Most Popular";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(52, 290);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 24);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Latest";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(52, 388);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 24);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Finalize";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Bookman Old Style", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(112, 491);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 31);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Log Out";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Bookman Old Style", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(581, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(146, 28);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Book Shop";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Most_popular_books.Properties.Resources.book_user__1_;
+            this.pictureBox4.Location = new System.Drawing.Point(315, 117);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(34, 36);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Most_popular_books.Properties.Resources.books__1_;
+            this.pictureBox3.Location = new System.Drawing.Point(620, 63);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(37, 35);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Most_popular_books.Properties.Resources.logout__1_;
+            this.pictureBox2.Location = new System.Drawing.Point(25, 491);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(37, 31);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Most_popular_books.Properties.Resources.book_shop__2___1_;
+            this.pictureBox1.Location = new System.Drawing.Point(10, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(68, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Bookman Old Style", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(365, 127);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(115, 22);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "User Name";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.B_ID,
+            this.B_TITLE,
+            this.B_AUTHOR,
+            this.B_CATEGORY,
+            this.B_PRICE,
+            this.CHOISE});
+            this.dataGridView1.GridColor = System.Drawing.Color.SteelBlue;
+            this.dataGridView1.Location = new System.Drawing.Point(315, 173);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(617, 399);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // B_ID
+            // 
+            this.B_ID.HeaderText = "B_ID";
+            this.B_ID.Name = "B_ID";
+            // 
+            // B_TITLE
+            // 
+            this.B_TITLE.HeaderText = "B_TITLE";
+            this.B_TITLE.Name = "B_TITLE";
+            // 
+            // B_AUTHOR
+            // 
+            this.B_AUTHOR.HeaderText = "B_AUTHOR";
+            this.B_AUTHOR.Name = "B_AUTHOR";
+            // 
+            // B_CATEGORY
+            // 
+            this.B_CATEGORY.HeaderText = "B_CATEGORY";
+            this.B_CATEGORY.Name = "B_CATEGORY";
+            // 
+            // B_PRICE
+            // 
+            this.B_PRICE.HeaderText = "B_PRICE";
+            this.B_PRICE.Name = "B_PRICE";
+            // 
+            // CHOISE
+            // 
+            this.CHOISE.HeaderText = "CHOISE";
+            this.CHOISE.Name = "CHOISE";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(864, 196);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(59, 20);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "ADD";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(235)))), ((int)(((byte)(240)))));
+            this.ClientSize = new System.Drawing.Size(958, 594);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn B_ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn B_TITLE;
+        private System.Windows.Forms.DataGridViewTextBoxColumn B_AUTHOR;
+        private System.Windows.Forms.DataGridViewTextBoxColumn B_CATEGORY;
+        private System.Windows.Forms.DataGridViewTextBoxColumn B_PRICE;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CHOISE;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Most_popular_books/Most_popular_books/Most_popular_books/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Most_popular_books
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Assuming you have fetched your data into a DataTable
+            DataTable salesData = GetSalesData();
+
+            // Sort the data by sales quantity or revenue column (assuming it's a numeric column)
+            DataView sortedView = salesData.DefaultView;
+            sortedView.Sort = "SalesQuantity DESC"; // Change SalesQuantity to the appropriate column name
+
+            // Select the top 10 records
+            DataTable top10Data = sortedView.ToTable().AsEnumerable().Take(10).CopyToDataTable();
+
+            // Bind the top 10 data to the DataGridView
+            dataGridView1.DataSource = top10Data;
+        }
+
+        // Method to fetch sample sales data
+        private DataTable GetSalesData()
+        {
+            // Replace this with your actual data retrieval logic
+            DataTable salesData = new DataTable();
+            // Assume columns are SalesID, ProductName, SalesQuantity, Revenue
+            salesData.Columns.Add("B_ID", typeof(int));
+            salesData.Columns.Add("B_TITLE", typeof(string));
+            salesData.Columns.Add("B_AUTHOR", typeof(string));
+            salesData.Columns.Add("B_CATEGORY", typeof(string));
+            salesData.Columns.Add("B_PRICE", typeof(decimal));
+
+            // Fill in some sample data
+            // Add your actual data here
+            salesData.Rows.Add(1, "Product A", 100, 500.00);
+            salesData.Rows.Add(2, "Product B", 150, 750.00);
+            // Add more rows as needed
+
+            return salesData;
+        }
+    }
+}
